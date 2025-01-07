b1207f0cf4cc5c37fa3d19be270b8f80354804c7
Nahid <syed.ah.nahid@gmail.com>
2025-01-07 18:54:33 +0600

    created the theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
+import 'package:expense_tracker/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
     runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-    const MyApp({super.key});
-
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-            home: Scaffold(
-                body: Center(
-                    child: Text("hello"),
-                ),
-            ),
-        );
-    }
 }
